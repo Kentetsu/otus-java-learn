b48c96d3604bd9487605bd67723a2c47c1dedf6c
Андрей <gambitonius@gmail.com>
2025-07-04 22:02:10 +0300

    add template project

---
+++ Eon/src/main/java/ru/eon/App.java
@@ -0,0 +1,23 @@
+package ru.eon;
+
+import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String... args) {
+
+        List<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 100;
+        for (int i = min; i < max; i++) {
+            example.add(i);
+        }
+
+        logger.info(Lists.reverse(example).toString());
+    }
+}
